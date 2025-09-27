f11e26df247ea3c538da722b20cee93691f475d6
JonathanLGM <JLOPEZG105907@umanizales.edu.co>
2025-09-27 17:48:52 -0500

    cuenta

--- rutasCuenta.js
+++ rutasCuenta.js
@@ -6,7 +6,7 @@ const cuentaControlador = require('./cuentaControlador'); // ahora todo está en
 router.post('/', cuentaControlador.registrarCuenta);
 
 // Obtener todas las cuentas
-router.get('/', cuentaControlador.listarCuentas);
+router.get('/listarCuentas', cuentaControlador.listarCuentas);
 
 // Obtener una cuenta por id
 router.get('/:id_cuenta', cuentaControlador.obtenerCuentaPorId);
